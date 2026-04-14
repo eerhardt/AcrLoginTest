841200530b76a0615823903b66bb05a2db7d1939
Eric Erhardt <eric.erhardt@microsoft.com>
2026-04-14 10:48:33 -0500

    Add ACA Fix versions

--- AcrLoginTest.AppHost/AppHost.cs
+++ AcrLoginTest.AppHost/AppHost.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddAzureContainerAppEnvironment("env");
+
 builder.AddProject<Projects.AcrLoginTest_Api>("api")
     .WithExternalHttpEndpoints();
 
